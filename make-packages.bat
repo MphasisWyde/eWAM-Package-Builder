bcaa6b621e1e842bc024e6e6395b43aa5b3b5e87
Redgis M <regis.martinez@mphasis.com>
2018-11-28 18:12:24 +0100

    Added sample commandline for ClickOnce / OCX indexing

--- make-packages.bat
+++ make-packages.bat
@@ -3,6 +3,7 @@ set destination=C:\inetpub\wwwroot\eWamUpdate
 cd /d %~dp0
 
 python packagebuilder.py D:\wyde\ --package-index-policy overwrite --deploy %destination% --wipe-destination
+python packagebuilder.py C:\inetpub\wwwroot --package-index-policy append --deploy %destination%
 
 REM python packagebuilder.py D:\wyde\eWAM --package-index-policy overwrite --deploy %destination% --wipe-destination
 REM python packagebuilder.py D:\wyde\Wynsure --package-index-policy append --deploy %destination%
